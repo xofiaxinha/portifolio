4999df67e6833a716e2239416ca142fa18b7751e
xofiaxinha <msofia.lima@alu.ufc.br>
2025-03-06 17:49:01 -0300

    ta vo parar de bulir nos projetos

--- src/components/projetos/projetos.jsx
+++ src/components/projetos/projetos.jsx
@@ -5,14 +5,14 @@ import "./assets/projetos.css"
 function Projetos({listaProjetos}){
     const [hoveredIndex, setHoveredIndex] = useState(-1)
     return(
-        <Container>
+        <Container className="flex items-center">
             <h1 className="text-amber-950 text-5xl font-bold uppercase lg:text-6xl my-2">Projetos</h1>
-            <div className="projetos-container flex flex-wrap justify-center">
+            <div className="projetos-container w-full p-10 flex flex-wrap justify-center">
                 {listaProjetos.map((projeto, index) => {
                     return(
                         <div 
                             key={index} 
-                            className={`item w-70 h-80 bg-amber-500 flex items-end rounded-lg ${hoveredIndex !== null && hoveredIndex !== index ? 'squished' : ''}`}
+                            className={`item h-50 bg-amber-500 flex items-end rounded-lg ${hoveredIndex !== null && hoveredIndex !== index ? 'squished' : ''}`}
                             onMouseEnter={() => setHoveredIndex(index)}
                             onMouseLeave={() => setHoveredIndex(-1)}
                         >
