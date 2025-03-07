b48a3250f490ee093c5894df1dba33c92cc6f091
xofiaxinha <msofia.lima@alu.ufc.br>
2025-03-07 14:21:52 -0300

    footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return(
+        <footer className="w-full bg-amber-600 text-white text-center p-8 items-center justify-center">
+            <h1 className="uppercase text-4xl">Obrigado!</h1>
+            <h3 className="text-l">desenvolvido por Maria Sofia</h3>
+            <h3 className="text-l">icons por <a href="">icons8</a></h3>
+        </footer>
+    )
+}
