9063f9553f97dd1f868275241435977a229a2487
xofiaxinha <msofia.lima@alu.ufc.br>
2025-03-24 17:15:44 -0300

    adição de imagens

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -6,7 +6,8 @@ export default function Home(){
         <Container className="justify-center items-center my-4">
             <h1 className="text-amber-950 text-5xl font-bold uppercase lg:text-6xl my-2">Maria Sofia</h1>
             <h3 className="text-amber-700 text-xl tracking-[.40em] lg:text-2xl my-2">front-end, UI/UX</h3>
-            <div className="rounded-full w-70 lg:w-100 h-70 lg:h-100 bg-amber-700 m-4"></div>
+            <div className="rounded-full w-70 lg:w-120 h-70 lg:h-120 bg-[url(src/assets/img_1.png)] bg-cover bg-amber-700 m-4">
+            </div>
         </Container>
     )
 }
--- src/components/sobre/sobre.jsx
+++ src/components/sobre/sobre.jsx
@@ -8,12 +8,17 @@ export default function Sobre(){
     return(
         <Container className="flex flex-col md:flex-row lg:p-20">
             <div className="img-section flex w-full justify-center">
-                <div className="rounded-full w-60 lg:w-100 h-60 lg:h-100 bg-amber-700 m-4 col-span-1"></div>
+                <div className="rounded-full w-70 lg:w-120 h-70 lg:h-120 bg-[url(src/assets/img_5.png)] bg-cover bg-amber-700 m-4 col-span-1"></div>
             </div>
             <div className="sobre-section flex flex-col w-full justify-center">
                 <h1 className="text-amber-950 text-5xl font-bold uppercase lg:text-6xl my-2">Maria Sofia</h1>
                 <h3 className="text-amber-700 text-xl tracking-[.40em] lg:text-2xl my-2">front-end, UI/UX</h3>
-                <p className="text-amber-900 text-lg my-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec justo eget felis facilisis fermentum. Nullam nec turpis</p>
+                <p className="text-amber-900 text-lg my-2">Estudante de Ciência da Computação com um grande interesse por arte e design, 
+                    vejo na programação uma nova forma de poder me expressar, além de poder transmitir informações para os usuários de forma
+                    responsiva e amigável, sempre buscando prover a melhor experiência que eu posso entregar.</p>
+                <p className="text-amber-900 text-lg my-2">Para além da programação, gosto muito de desenho digital e jogos (inclusive, você pode ver a minha sim aqui do lado).
+                    Sempre estou buscando aprender coisas novas e me desafiar, e estou sempre aberta a novas oportunidades e projetos.
+                </p>
                 <div className="button-container flex w-full gap-4">
                     <IconButton icon={linkedin} link="https://www.linkedin.com/in/xofiaxinha/"/>
                     <IconButton icon={github} link="https://github.com/xofiaxinha/"/>
